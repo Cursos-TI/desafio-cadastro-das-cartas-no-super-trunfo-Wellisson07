f8046d0e7726d5bab240cbe28ac4ba8b12d016c7
Wellisson07 <wellissonlucas0073@gmail.com>
2025-04-09 22:19:03 +0000

    Novo commit

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,96 +1,34 @@
 #include <stdio.h>
 
 int main() {
-  
-    char estado[50], cidade[50];
-    int codigoCarta, populacao, pontosTuristicos;
-    float area, pib;
-
-
-    printf("Digite o nome do estado: ");
-    fgets(estado, sizeof(estado), stdin); 
-    estado[strcspn(estado, "\n")] = '\0'; 
-
-    printf("Digite o código da carta do estado: ");
-    scanf("%d", &codigoCarta);
-
-    printf("Digite o nome da cidade: ");
-    getchar(); 
-    fgets(cidade, sizeof(cidade), stdin);
-    cidade[strcspn(cidade, "\n")] = '\0'; 
-
-    printf("Digite a população da cidade: ");
-    scanf("%d", &populacao);
-
-    printf("Digite a área da cidade em km²: ");
-    scanf("%f", &area);
-
-    printf("Digite o PIB da cidade em milhões: ");
-    scanf("%f", &pib);
-
-    printf("Digite o número de pontos turísticos: ");
-    scanf("%d", &pontosTuristicos);
-
-
-    printf("\nDados do Estado e Cidade:\n");
-    printf("Estado: %s\n", estado);
-    printf("Código da Carta: %d\n", codigoCarta);
-    printf("Cidade: %s\n", cidade);
-    printf("População: %d\n", populacao);
-    printf("Área: %.2f km²\n", area);
-    printf("PIB: %.2f milhões\n", pib);
-    printf("Número de pontos turísticos: %d\n", pontosTuristicos);
     
+    float populacao = 2686612;  
+    float area = 313;           
+    float pib = 61700000000;    
 
-    char estado[50], cidade[50];
-    int codigoCarta, populacao, pontosTuristicos;
-    float area, pib, idh, densidadePopulacional;
+    float densidade_populacional, pib_per_capita;
+
+    densidade_populacional = populacao / area;
+
+    pib_per_capita = pib / populacao;
+
+    printf("Densidade Populacional de Fortaleza: %.2f habitantes por km²\n", densidade_populacional);
+    printf("PIB per Capita de Fortaleza: %.2f reais por pessoa\n", pib_per_capita);
 
     
-    printf("Digite o nome do estado: ");
-    fgets(estado, sizeof(estado), stdin); 
-    estado[strcspn(estado, "\n")] = '\0'; 
+    float populacao = 2923182;  
+    float area = 713.43;        
+    float pib = 89000000000;   
 
-    printf("Digite o código da carta do estado: ");
-    scanf("%d", &codigoCarta);
+    float densidade_populacional, pib_per_capita;
 
-    printf("Digite o nome da cidade: ");
-    getchar(); 
-    fgets(cidade, sizeof(cidade), stdin);
-    cidade[strcspn(cidade, "\n")] = '\0'; 
+    densidade_populacional = populacao / area;
 
-    printf("Digite a população da cidade: ");
-    scanf("%d", &populacao);
+    pib_per_capita = pib / populacao;
 
-    printf("Digite a área da cidade em km²: ");
-    scanf("%f", &area);
-
-    printf("Digite o PIB da cidade em milhões: ");
-    scanf("%f", &pib);
-
-    printf("Digite o número de pontos turísticos: ");
-    scanf("%d", &pontosTuristicos);
-
-    densidadePopulacional = populacao / area;
-   
-    idh = pib / (populacao * 1000);
-
-    printf("\nDados do Estado e Cidade:\n");
-    printf("Estado: %s\n", estado);
-    printf("Código da Carta: %d\n", codigoCarta);
-    printf("Cidade: %s\n", cidade);
-    printf("População: %d\n", populacao);
-    printf("Área: %.2f km²\n", area);
-    printf("PIB: %.2f milhões\n", pib);
-    printf("Número de pontos turísticos: %d\n", pontosTuristicos);
-    printf("Densidade Populacional: %.2f habitantes por km²\n", densidadePopulacional);
-    printf("Índice de Desenvolvimento Humano (IDH) simplificado: %.4f\n", idh);
-    printf("Novo commit\n");
-    
+    printf("Densidade Populacional de Salvador: %.2f habitantes por km²\n", densidade_populacional);
+    printf("PIB per Capita de Salvador: %.2f reais por pessoa\n", pib_per_capita);
 
     return 0;
-
-
 }
-
 
